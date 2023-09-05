a4d5dd519c718b846fd8d453b4d4ce4b7d22dc86
jenina <Learning@Jeninas-MacBook-Air.local>
2023-09-06 07:10:39 +0800

    change routing to latest laravel with class method

--- routes/web.php
+++ routes/web.php
@@ -1,98 +1,67 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\PermissionsController;
+use App\Http\Controllers\RolesController;
+use App\Http\Controllers\DocumentsController;
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\TasksController;
+use App\Http\Controllers\MailboxController;
+use App\Http\Controllers\CalendarController;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
-
+Route::middleware(['auth'])->prefix('admin')->group(function () {
     Route::get('/', function () {
         return view('pages.home.index');
     });
 
-    Route::resource('/users', 'App\Http\Controllers\UsersController');
+    Route::resource('users', UsersController::class);
+    Route::get('my-profile', [UsersController::class, 'getProfile']);
+    Route::get('my-profile/edit', [UsersController::class, 'getEditProfile']);
+    Route::patch('my-profile/edit', [UsersController::class, 'postEditProfile']);
 
-    Route::get('/my-profile', 'App\Http\Controllers\UsersController@getProfile');
+    Route::resource('permissions', PermissionsController::class);
+    Route::resource('roles', RolesController::class);
+    Route::get('users/role/{id}', [UsersController::class, 'getRole']);
+    Route::put('users/role/{id}', [UsersController::class, 'updateRole']);
 
-    Route::get('/my-profile/edit', 'App\Http\Controllers\UsersController@getEditProfile');
+    Route::resource('documents', DocumentsController::class);
+    Route::get('documents/{id}/assign', [DocumentsController::class, 'getAssignDocument']);
+    Route::put('documents/{id}/assign', [DocumentsController::class, 'postAssignDocument']);
 
-    Route::patch('/my-profile/edit', 'App\Http\Controllers\UsersController@postEditProfile');
+    Route::resource('contacts', ContactsController::class);
+    Route::get('contacts/{id}/assign', [ContactsController::class, 'getAssignContact']);
+    Route::put('contacts/{id}/assign', [ContactsController::class, 'postAssignContact']);
+    Route::get('api/contacts/get-contacts-by-status', [ContactsController::class, 'getContactsByStatus']);
 
-    Route::resource('/permissions', 'App\Http\Controllers\PermissionsController');
+    Route::resource('tasks', TasksController::class);
+    Route::get('tasks/{id}/assign', [TasksController::class, 'getAssignTask']);
+    Route::put('tasks/{id}/assign', [TasksController::class, 'postAssignTask']);
+    Route::get('tasks/{id}/update-status', [TasksController::class, 'getUpdateStatus']);
+    Route::put('tasks/{id}/update-status', [TasksController::class, 'postUpdateStatus']);
 
-    Route::resource('/roles', 'App\Http\Controllers\RolesController');
+    Route::resource('mailbox', MailboxController::class)->parameter('mailbox', 'folder');
+    Route::get('mailbox-create', [MailboxController::class, 'create']);
+    Route::post('mailbox-create', [MailboxController::class, 'store']);
+    Route::get('mailbox-show/{id}', [MailboxController::class, 'show']);
+    Route::put('mailbox-toggle-important', [MailboxController::class, 'toggleImportant']);
+    Route::delete('mailbox-trash', [MailboxController::class, 'trash']);
+    Route::get('mailbox-reply/{id}', [MailboxController::class, 'getReply']);
+    Route::post('mailbox-reply/{id}', [MailboxController::class, 'postReply']);
+    Route::get('mailbox-forward/{id}', [MailboxController::class, 'getForward']);
+    Route::post('mailbox-forward/{id}', [MailboxController::class, 'postForward']);
+    Route::get('mailbox-send/{id}', [MailboxController::class, 'send']);
 
-    Route::get('/users/role/{id}', 'App\Http\Controllers\UsersController@getRole');
+    Route::get('calendar', [CalendarController::class, 'index']);
 
-    Route::put('/users/role/{id}', 'App\Http\Controllers\UsersController@updateRole');
-
-    Route::resource('/documents', 'App\Http\Controllers\DocumentsController');
-
-    Route::get('/documents/{id}/assign', 'App\Http\Controllers\DocumentsController@getAssignDocument');
-
-    Route::put('/documents/{id}/assign', 'App\Http\Controllers\DocumentsController@postAssignDocument');
-
-    Route::resource('/contacts', 'App\Http\Controllers\ContactsController');
-
-    Route::get('/contacts/{id}/assign', 'App\Http\Controllers\ContactsController@getAssignContact');
-
-    Route::put('/contacts/{id}/assign', 'App\Http\Controllers\ContactsController@postAssignContact');
-
-    Route::get('/api/contacts/get-contacts-by-status', 'App\Http\Controllers\ContactsController@getContactsByStatus');
-
-    Route::resource('/tasks', 'App\Http\Controllers\TasksController');
-
-    Route::get('/tasks/{id}/assign', 'App\Http\Controllers\TasksController@getAssignTask');
-
-    Route::put('/tasks/{id}/assign', 'App\Http\Controllers\TasksController@postAssignTask');
-
-    Route::get('/tasks/{id}/update-status', 'App\Http\Controllers\TasksController@getUpdateStatus');
-
-    Route::put('/tasks/{id}/update-status', 'App\Http\Controllers\TasksController@postUpdateStatus');
-
-    Route::get('/mailbox/{folder?}', 'App\Http\Controllers\MailboxController@index');
-
-    Route::get('/mailbox-create', 'App\Http\Controllers\MailboxController@create');
-
-    Route::post('/mailbox-create', 'App\Http\Controllers\MailboxController@store');
-
-    Route::get('/mailbox-show/{id}', 'App\Http\Controllers\MailboxController@show');
-
-    Route::put('/mailbox-toggle-important', 'App\Http\Controllers\MailboxController@toggleImportant');
-
-    Route::delete('/mailbox-trash', 'App\Http\Controllers\MailboxController@trash');
-
-    Route::get('/mailbox-reply/{id}', 'App\Http\Controllers\MailboxController@getReply');
-
-    Route::post('/mailbox-reply/{id}', 'App\Http\Controllers\MailboxController@postReply');
-
-    Route::get('/mailbox-forward/{id}', 'App\Http\Controllers\MailboxController@getForward');
-
-    Route::post('/mailbox-forward/{id}', 'App\Http\Controllers\MailboxController@postForward');
-
-    Route::get('/mailbox-send/{id}', 'App\Http\Controllers\MailboxController@send');
-
-    Route::get('/calendar', 'App\Http\Controllers\CalendarController@index');
-
-    Route::get('/forbidden', function () {
+    Route::get('forbidden', function () {
         return view('pages.forbidden.forbidden_area');
     });
 });
 
 Route::get('/', function () {
-   return redirect()->to('/admin');
+    return redirect('/admin');
 });
 
 Auth::routes();
-
-//comment
